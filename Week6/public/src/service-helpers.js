d4cb43bc270e55e854cafa7b1302b6b4ea445d92
boozie4 <eboozell40@gmail.com>
2025-02-23 13:17:03 -0600

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   Week5/public/index.html
    	renamed:    Week5/public/src/app.js -> Week5/public/src/auth.js
    	modified:   Week6/package-lock.json
    	modified:   Week6/package.json
    	modified:   Week6/public/home.html
    	modified:   Week6/public/index.html
    	renamed:    Week6/public/register.html -> Week6/public/login.html
    	new file:   Week6/public/src/add-task.js
    	new file:   Week6/public/src/api.config.js
    	renamed:    Week6/public/src/app.js -> Week6/public/src/auth.js
    	modified:   Week6/public/src/build-tasks-list.js
    	new file:   Week6/public/src/route-guard.js
    	modified:   Week6/public/src/service-helpers.js
    	new file:   Week6/public/src/simple-storage.js
    	new file:   Week6/public/src/tasks.service.js

--- Week6/public/src/service-helpers.js
+++ Week6/public/src/service-helpers.js
@@ -1,25 +1,82 @@
-function _get(url) {
-    return fetch(url, {
-        method: 'GET'
-    });
-}
+const access_token = storageHasData() ? getStorage('access_token') : '';
+const token = `Bearer ${access_token}`;
 
-function _post(url, data) {
-    return fetch(url, {
+const DEFAULT_OPTIONS = {
+    headers: {
+        'Content-Type': 'application/json',
+    },
+};
+
+const DEFAULT_OPTIONS_WITH_AUTH = {
+    headers: {
+        Authorization: token,
+        'Content-Type': 'application/json',
+    },
+};
+
+const OPTIONS_WITH_AUTH = {
+    headers: {
+        Authorization: token,
+    },
+};
+
+/**
+ * Generic Read API Handler
+ * 
+ * @param {string} url - address to make request to
+ * @param {any} options - additional options to send. Defaults to options with auth headers
+ */
+const _get = async (url, options = DEFAULT_OPTIONS_WITH_AUGH) => {
+    const res = await fetch(url, {
+        method: 'GET',
+        ... options,
+    });
+    return res.json();
+};
+
+/**
+ * Generic Create API Handler
+ * 
+ * @param {string} url - address to make request to 
+ * @param {any} data - updates to send
+ * @param {any} options - additional options to send. Defaults to options with normal headers
+ */
+const _post = async (url, data, options = DEFAULT_OPTIONS) => {
+    const res = await fetch(url, {
         method: 'POST',
-        headers: {
-        'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
+        ... options,
+        body: JSON.stringify(data),
     });
-}
 
-function _put(url, data) {
-    return fetch(url, {
+    return res.json();
+};
+
+/**
+ * Generic Update API Handler
+ * NOTE: PUT requests strictly require authentication.
+ * @param {string} url - address to make requests to 
+ * @param {any} data - updates to send
+ * @param {any} options - additional options to send. Defaults to options with auth headers
+ */
+const _put = async (url, data, options = DEFAULT_OPTIONS_WITH_AUTH) => {
+    const res = await fetch(url, {
         method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
+        ... options,
+        body: JSON.stringify(data),
     });
-}
+};
+
+/**
+ * Generic Delete API Handler
+ * NOTE: DELETE requests strictly require authentication
+ * 
+ * @param {string} url - address to make request to 
+ * @param {any} options - additional options to send. Defaults to options with auth headers
+ */
+const _delete = async (url, options = DEFAULT_OPTIONS_WITH_AUTH) => {
+    const res = await fetch(url, {
+        method: 'DELETE',
+        ... options,
+    });
+    return res.json();
+};
